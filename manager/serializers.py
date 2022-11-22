1111dd1c06141e5ca86d3b9679962c14f39a3b53
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-11-22 16:00:18 +0330

    Refactor create_run

--- manager/serializers.py
+++ manager/serializers.py
@@ -25,11 +25,36 @@ class RunAppSerializer(serializers.ModelSerializer):
         app = App.objects.get(name=self.context['name'])
         docker_manager = DockerManger(name=app.name, image=app.image, envs=app.envs, command=app.command)
         container_id = docker_manager.run()
-        run = {
-            "state": "R",
-            "envs": app.envs,
-            "command": app.command,
-            "app_id": app,
-            "container_id": container_id
-        }
-        return Run.objects.create(**run)
+        return Run.objects.create(
+            state='R',
+            envs=app.envs,
+            command=app.command,
+            app_id=app,
+            container_id=container_id
+        )
+
+
+class AppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = "__all__"
+
+
+class RunSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Run
+        fields = "__all__"
+
+
+class AppAndContainerSerializer(serializers.ModelSerializer):
+    # run_set = RunSerializer(many=True)
+    run_set = serializers.SerializerMethodField('get_containers')
+
+    class Meta:
+        model = App
+        fields = ('id', 'name', 'image', 'envs', 'command', 'run_set')
+
+    def get_containers(self, instance):
+        run_queryset = Run.objects.filter(app_id=self.instance.id, state='F')
+        serialized = RunSerializer(instance=run_queryset, many=True)
+        return serialized.data
