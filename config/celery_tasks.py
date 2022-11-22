bf001e19d889dc46e62a4b28c0df8efece9a0376
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-11-22 14:56:21 +0330

    Complete docker_events in celery task

--- config/celery_tasks.py
+++ config/celery_tasks.py
@@ -2,6 +2,8 @@ from config.celery import app
 from config.settings import client
 from celery.signals import celeryd_init
 
+from manager.models import Run
+
 
 class CeleryTasks:
     @staticmethod
@@ -17,8 +19,13 @@ class CeleryTasks:
     @staticmethod
     @app.task
     def docker_events():
+        finish_statuses = ['die', 'destroy', 'kill', 'stop']
         for event in client.events(decode=True):
-            print(event)
+            if event.get('status') in finish_statuses:
+                container_id = event.get('id')
+                run = Run.objects.get(container_id=container_id)
+                Run.objects.create(state='F', envs=run.envs, command=run.command ,app_id=run.app_id , container_id=container_id)
+
 
 
 @celeryd_init.connect
