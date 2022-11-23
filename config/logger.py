6114485bf6e40e94c0ef3c8821aede6d2db4b52d
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-11-23 12:34:14 +0330

    Refactor logger file

--- config/logger.py
+++ config/logger.py
@@ -1,26 +1,11 @@
 import logging
-import traceback
 
 logger = logging.getLogger(__name__)
 
 
-def log_message(*, error, request):
-
-    if request:
-        message = '{error}'.format(
-            error=error
-        )
-    else:
-        message = '{error}'.format(error=error)
-    return message
+def log_error(message):
+    logger.error(message)
 
 
-def log_error(message, *, request=None):
-    _log_message = log_message(error=message, request=request)
-    logger.error(_log_message)
-
-
-def log_warning(message, *, request=None):
-    _log_message = log_message(error=message, request=request)
-    logger.warning(_log_message)
-
+def log_info(message):
+    logger.info(message)
